4dee4678c984a7df030f84e2c3ca4a59f2e093dc
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-09-20 16:21:01 +0530

    Create Question_1.py

---
+++ Sept_20_2025/Question_1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# reshape is used to create an array of nxm dimensions
+arr = np.arange(1, 17).reshape(4, 4)
+
+print(arr)
+print("")
+
+# ravel is used to convert nxm dimension array to 1D
+arr = np.ravel(arr)
+
+print(arr)
