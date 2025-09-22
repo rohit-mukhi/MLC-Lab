7f20559403e356aafd07aa297c02a48e0a3ccf6d
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-09-22 12:36:46 +0530

    Update Question_3.py

--- Sept_22_2025/Question_3.py
+++ Sept_22_2025/Question_3.py
@@ -4,9 +4,18 @@ import numpy as np
 arr1 = np.array([1, 2, 3, 4])
 arr2 = np.array([35, 34, 33, 32])
 
+arr3 = np.array([15, 15, 15])
+
 x1 = pd.Series(arr1)
 x2 = pd.Series(arr2)
+x3 = pd.Series(arr3)
 
+# Simply adding cardinally alike series will retain their datatype
 my_sum = pd.Series(x1 + x2)
 
+# when adding less elements, the outputs are stored as floats
+new_sum = pd.Series(x1 + x2 + x3)
+
 print(my_sum)
+print("")
+print(new_sum)
