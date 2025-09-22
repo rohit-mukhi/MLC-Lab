50d0fb0037568b484556d08196b58e68299a5569
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-09-22 12:56:33 +0530

    Update Question_4.py

--- Sept_22_2025/Question_4.py
+++ Sept_22_2025/Question_4.py
@@ -5,6 +5,13 @@ arr1 = np.array([12, 24, 36, 48, 60, 72])
 arr2 = np.array([17, 34, 51, 68, 85, 102])
 
 x = pd.Series(arr1)
+y = pd.Series(arr2)
 
 # The head() function will print the first 5 elements
 print(x.head())
+print("")
+print(y.head())
+
+# To print the size of a series
+# Look for this one...
+print(x.Size())
