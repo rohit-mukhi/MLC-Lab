673c2ff8dc2ddbcdaee4507a9f6d5bfccc34c64d
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2026-02-23 12:13:43 +0530

    Update main.py

--- 23_Februray_2026/main.py
+++ 23_Februray_2026/main.py
@@ -42,3 +42,37 @@ def confidence(X, Y, transactions):
 print("Support (Brand): ", support({"Bread"}, transactions))
 print("Confidence (Bread-Milk): ", confidence({"Bread"}, {"Milk"}, transactions))
 print("Confidence (Bread-Milk-Egg): ", confidence({"Bread","Milk"}, {"Egg"}, transactions))
+
+min_support = 0.5
+
+"""
+
+Support function
+
+"""
+
+def support(itemset):
+    return sum(1 for t in transactions if itemset.issubset(t) / len(transactions))
+
+"""
+
+Apriori Principle Demonstration
+
+"""
+
+items = sorted(set().union(*transactions))
+freq_t = [ {i} for i in items if support({i}) >= min_support ]
+
+print("Frequent 1-itemsets: ")
+for i in freq_t:
+    print(i, "Support = ", support({i}))
+    
+from itertools import combinations
+
+candidates_2 = [ set(c) for c in combinations([list[1][0] for i in freq_t], 2) ]
+
+freq_2 = [ c for c in candidates_2 if support(c) >= min_support ]
+
+print("\nFrequent 2-itemsets (after Apriori pruning): ")
+for c in freq_2:
+    print(c, "Suport = ", support(c))
